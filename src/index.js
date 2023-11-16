fecec4379880c710f712dca6fea16bf2d536c28d
Joce <jocelynn4355@gmail.com>
2023-11-15 23:50:57 -0600

    minor changes to content in components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+// import './index.css';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import './index.css';
 // here is where we start creating the DOM, then rendering the App to the UI
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
